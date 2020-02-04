a020598800d08ad4ae0c7890c8e986d0355f80f2
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-02-04 16:11:11 -0500

    [Fix] add if anem main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,26 +5,26 @@ to gather info about employee"""
 import requests
 import sys
 
+if __name__ == "__main__":
+    parameters_t = (('userId', sys.argv[1]),)
+    to_dos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos',
+        params=parameters_t)
+    to_dos_j = to_dos.json()
 
-parameters_t = (('userId', sys.argv[1]),)
-to_dos = requests.get(
-    'https://jsonplaceholder.typicode.com/todos',
-    params=parameters_t)
-to_dos_j = to_dos.json()
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
+    user_j = user.json()
+    user_name = user_j['name']
 
-user = requests.get(
-    'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
-user_j = user.json()
-print(user_j)
-user_name = user_j['name']
+    completed_list = []
+    for maybe in to_dos_j:
+        if maybe['completed'] is True:
+            completed_list.append(maybe)
 
-completed_list = []
-for maybe in to_dos_j:
-    if maybe['completed'] is True:
-        completed_list.append(maybe)
-
-print("Employee {} is done with tasks({}/{}):".format(user_name,
-                                                      len(completed_list),
-                                                      len(to_dos_j)))
-for title in completed_list:
-    print("\t {}".format(title['title']))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name,
+        len(completed_list),
+        len(to_dos_j)))
+    for title in completed_list:
+        print("\t {}".format(title['title']))
